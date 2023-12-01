35fa8cbb4ccc18f185c39f6982439568fac25b69
IXorusI <astartes93@gmail.com>
2023-12-01 16:56:57 +0300

    1.4

--- scripts/index.js
+++ scripts/index.js
@@ -2,29 +2,42 @@ const content = document.querySelector('.content');
 const placesSection = content.querySelector('.places');
 const placeList = placesSection.querySelector('.places__list');
 const places = Array.from(initialCards);
-// @todo: Темплейт карточки
+
 const profileTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
-// @todo: Функция создания карточки
-function addCard(name, link) {
+// function createCard(name, link) {
+//     const cardElement = profileTemplate.querySelector('.places__item').cloneNode(true);
+//     const delButton = cardElement.querySelector('.card__delete-button');
+
+//     cardElement.querySelector('.card__title').textContent = name;
+//     cardElement.querySelector('.card__image').src = link;
+//     cardElement.querySelector('.card__image').alt = `Фото ${name}`;
+
+//     delButton.addEventListener('click', handleDeleteCard);
+    
+//     placeList.append(cardElement);
+// }
+
+function createCard(card, callback) {
     const cardElement = profileTemplate.querySelector('.places__item').cloneNode(true);
     const delButton = cardElement.querySelector('.card__delete-button');
 
-    cardElement.querySelector('.card__title').textContent = name;
-    cardElement.querySelector('.card__image').src = link;
-    cardElement.querySelector('.card__image').alt = `Фото ${name}`;
+    cardElement.querySelector('.card__title').textContent = card.name;
+    cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__image').alt = `Фото ${card.name}`;
 
-    delButton.addEventListener('click', delCard);
+    delButton.addEventListener('click', callback);
     
-    placeList.append(cardElement);
+    return cardElement;
 }
 
-// @todo: Функция удаления карточки
-function delCard(event) {
+function handleDeleteCard(event) {
     const card = event.target.closest('.places__item');
     card.remove();
 }
 
-// @todo: Вывести карточки на страницу
-places.forEach((item) => addCard(item.name, item.link))
+function renderCard (item) {
+    placeList.prepend(createCard(item, handleDeleteCard));
+}
+
+places.forEach((item) => renderCard(item))
